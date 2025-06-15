19d51b2894d2dc065cb34b5c6db348ff7cfe9f9a
Kilameen <kilameen@yandex.ru>
2025-06-15 14:45:54 +0300

    Выполение задания по ТЗ 16

--- server/src/test/java/ru/practicum/shareit/item/CommentCreateDtoTest.java
+++ server/src/test/java/ru/practicum/shareit/item/CommentCreateDtoTest.java
@@ -31,4 +31,36 @@ public class CommentCreateDtoTest {
         CommentCreateDto commentCreateDto = json.parse(jsonContent).getObject();
         assertThat(commentCreateDto.getText()).isEqualTo("Test comment");
     }
+
+    @Test
+    void testCommentCreateDtoSerialization_emptyText() throws IOException {
+        CommentCreateDto commentCreateDto = new CommentCreateDto();
+        commentCreateDto.setText("");
+        var jsonOutput = json.write(commentCreateDto);
+        assertThat(jsonOutput).hasJsonPath("$.text");
+        assertThat(jsonOutput).extractingJsonPathStringValue("$.text").isEqualTo("");
+    }
+
+    @Test
+    void testCommentCreateDtoDeserialization_emptyText() throws IOException {
+        String jsonContent = "{\"text\":\"\"}";
+        CommentCreateDto commentCreateDto = json.parse(jsonContent).getObject();
+        assertThat(commentCreateDto.getText()).isEqualTo("");
+    }
+
+    @Test
+    void testCommentCreateDtoSerialization_nullText() throws IOException {
+        CommentCreateDto commentCreateDto = new CommentCreateDto();
+        commentCreateDto.setText(null);
+        var jsonOutput = json.write(commentCreateDto);
+        assertThat(jsonOutput).hasJsonPath("$.text");
+        assertThat(jsonOutput.getJson()).contains("\"text\":null");
+    }
+
+    @Test
+    void testCommentCreateDtoDeserialization_nullText() throws IOException {
+        String jsonContent = "{\"text\":null}";
+        CommentCreateDto commentCreateDto = json.parse(jsonContent).getObject();
+        assertThat(commentCreateDto.getText()).isNull();
+    }
 }
--- server/src/test/java/ru/practicum/shareit/item/CommentDtoTest.java
+++ server/src/test/java/ru/practicum/shareit/item/CommentDtoTest.java
@@ -59,4 +59,132 @@ public class CommentDtoTest {
         assertThat(commentDto.getAuthorName()).isEqualTo("Test");
         assertThat(commentDto.getCreated()).isEqualTo(LocalDateTime.parse("2024-10-27T10:00:00"));
     }
+
+    @Test
+    void testCommentDtoSerializationNullValues() throws IOException {
+        CommentDto commentDto = CommentDto.builder().build();
+
+        JsonContent<CommentDto> result = json.write(commentDto);
+
+        assertThat(result).hasJsonPath("$.id");
+        assertThat(result).hasJsonPath("$.itemId");
+        assertThat(result).hasJsonPath("$.text");
+        assertThat(result).hasJsonPath("$.authorName");
+        assertThat(result).hasJsonPath("$.created");
+
+        assertThat(result).extractingJsonPathValue("$.id").isNull();
+        assertThat(result).extractingJsonPathValue("$.itemId").isNull();
+        assertThat(result).extractingJsonPathValue("$.text").isNull();
+        assertThat(result).extractingJsonPathValue("$.authorName").isNull();
+        assertThat(result).extractingJsonPathValue("$.created").isNull();
+    }
+
+    @Test
+    void testDeserializeCommentDtoEmptyString() throws IOException {
+        String jsonString = "{\"id\":1,\"itemId\":10,\"text\":\"\",\"authorName\":\"\",\"created\":\"2024-10-27T10:00:00\"}";
+
+        CommentDto commentDto = json.parse(jsonString).getObject();
+
+        assertThat(commentDto.getId()).isEqualTo(1L);
+        assertThat(commentDto.getItemId()).isEqualTo(10L);
+        assertThat(commentDto.getText()).isEqualTo("");
+        assertThat(commentDto.getAuthorName()).isEqualTo("");
+        assertThat(commentDto.getCreated()).isEqualTo(LocalDateTime.parse("2024-10-27T10:00:00"));
+    }
+
+    @Test
+    void testCommentDtoSerializationNullText() throws IOException {
+        CommentDto commentDto = CommentDto.builder()
+                .id(1L)
+                .itemId(10L)
+                .text(null)
+                .authorName("Test")
+                .created(LocalDateTime.now())
+                .build();
+        JsonContent<CommentDto> result = json.write(commentDto);
+        assertThat(result).extractingJsonPathValue("$.text").isNull();
+    }
+
+    @Test
+    void testDeserializeCommentDtoNullCreated() throws IOException {
+        String jsonString = "{\"id\":1,\"itemId\":10,\"text\":\"Test comment\",\"authorName\":\"Test\",\"created\":null}";
+
+        CommentDto commentDto = json.parse(jsonString).getObject();
+
+        assertThat(commentDto.getId()).isEqualTo(1L);
+        assertThat(commentDto.getItemId()).isEqualTo(10L);
+        assertThat(commentDto.getText()).isEqualTo("Test comment");
+        assertThat(commentDto.getAuthorName()).isEqualTo("Test");
+        assertThat(commentDto.getCreated()).isNull();
+    }
+
+    @Test
+    void testCommentDtoSerializationEmptyAuthorName() throws IOException {
+        CommentDto commentDto = CommentDto.builder()
+                .id(1L)
+                .itemId(10L)
+                .text("Test comment")
+                .authorName("")
+                .created(LocalDateTime.now())
+                .build();
+
+        JsonContent<CommentDto> result = json.write(commentDto);
+        assertThat(result).extractingJsonPathStringValue("$.authorName").isEqualTo("");
+    }
+
+    @Test
+    void testCommentDtoSerializationLongId() throws IOException {
+        CommentDto commentDto = CommentDto.builder().id(Long.MAX_VALUE).itemId(1L).text("text").authorName("name").created(LocalDateTime.now()).build();
+        JsonContent<CommentDto> result = json.write(commentDto);
+        assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(Long.MAX_VALUE);
+    }
+
+    @Test
+    void testCommentDtoDeserialization_invalidDate() throws IOException {
+        String jsonString = "{\"id\":1,\"itemId\":10,\"text\":\"Test comment\",\"authorName\":\"Test\",\"created\":\"invalid-date\"}";
+        try {
+            CommentDto commentDto = json.parse(jsonString).getObject();
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    void testCommentDtoSerialization_specialCharacters() throws IOException {
+        CommentDto commentDto = CommentDto.builder()
+                .id(1L)
+                .itemId(10L)
+                .text("Comment with special characters: !@#$%^&*()")
+                .authorName("Author with special characters: ~`")
+                .created(LocalDateTime.now())
+                .build();
+
+        JsonContent<CommentDto> result = json.write(commentDto);
+
+        assertThat(result).extractingJsonPathStringValue("$.text")
+                .isEqualTo("Comment with special characters: !@#$%^&*()");
+        assertThat(result).extractingJsonPathStringValue("$.authorName")
+                .isEqualTo("Author with special characters: ~`");
+    }
+
+    @Test
+    void testDeserializationWithMissingCreated() throws IOException {
+        String jsonString = "{\"id\":1,\"itemId\":10,\"text\":\"Test comment\",\"authorName\":\"Test\"}";
+        CommentDto commentDto = json.parse(jsonString).getObject();
+        assertThat(commentDto.getCreated()).isNull();
+    }
+
+    @Test
+    void testSerializationAuthorNameNull() throws IOException {
+        CommentDto commentDto = CommentDto.builder()
+                .id(1L)
+                .itemId(10L)
+                .text("Test")
+                .authorName(null)
+                .created(LocalDateTime.now())
+                .build();
+        JsonContent<CommentDto> result = json.write(commentDto);
+        assertThat(result).extractingJsonPathStringValue("$.authorName").isNull();
+
+    }
 }
